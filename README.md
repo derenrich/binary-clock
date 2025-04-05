24df65dcfb52e176d5bca5a51bf4599582449f2b
Daniel Erenrich <daniel@erenrich.net>
2025-04-05 07:18:59 -0700

    cleanup

---
+++ README.md
@@ -0,0 +1,3 @@
+Basic rust implementation of a clock using an rpico2040 written in Rust.
+
+Expects to have access to a DS3231MZ (for time keeping) and a IS31FL3236A-QFLS2-TR (for illumination) over I2C.
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,7 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_embedded_hal::shared_bus::blocking::i2c::I2cDevice;
 use embassy_rp::gpio;
-use embassy_time::Timer;
-use embassy_rp::interrupt;
 use embassy_rp::gpio::Pull;
 use embassy_rp::peripherals::{I2C0};
 
@@ -18,12 +15,8 @@ use gpio::{Level, Output, Input};
 use {defmt_rtt as _, panic_probe as _};
 use embassy_rp::i2c::{self, I2c, InterruptHandler};
 use ds323x::Ds323x;
-use ds323x::{NaiveDate, NaiveDateTime, DateTimeAccess, Timelike};
+use ds323x::{NaiveDateTime, DateTimeAccess, Timelike};
 use ds323x::Datelike;
-
-use embassy_sync::mutex::Mutex;
-use embassy_sync::blocking_mutex::raw::NoopRawMutex;
-
 
 use is31fl3236::{IS31FL3236, Is31fl32xx, SoftwareShutdownMode, OutputCurrent, OutputMode, GlobalEnable};
 
@@ -31,9 +24,7 @@ embassy_rp::bind_interrupts!(struct Irqs {
     I2C0_IRQ => InterruptHandler<embassy_rp::peripherals::I2C0>;
 });
 
-//static I2C_BUS: StaticCell<BusManager<I2c<'static, I2C0, i2c::Blocking>>> = StaticCell::new();
 static I2C_BUS: StaticCell<BusManagerSimple<I2c<'static, I2C0, i2c::Blocking>>> = StaticCell::new();
-
 
 const BUILD_UNIX_EPOCH: &str = env!("BUILD_UNIX_EPOCH");
 fn build_time() -> u64 {
@@ -49,15 +40,14 @@ async fn main(_spawner: Spawner) {
     let mut one_hz = Input::new(p.PIN_6, Pull::Up);
 
     let config = embassy_rp::i2c::Config::default();
-    let mut i2c = embassy_rp::i2c::I2c::new_blocking(p.I2C0, p.PIN_1, p.PIN_0, config);
+    let i2c = embassy_rp::i2c::I2c::new_blocking(p.I2C0, p.PIN_1, p.PIN_0, config);
 
     let bus = I2C_BUS.init(BusManagerSimple::new(i2c));
 
     let i2c_dev2 = bus.acquire_i2c();
-    let mut ledC = Is31fl32xx::<IS31FL3236, _>::init_with_i2c(0x00, i2c_dev2);
-    //ledC.set_shutdown(SoftwareShutdownMode::Normal).unwrap();
-    ledC.set_global_output(GlobalEnable::Enable).unwrap();
-    ledC.set_shutdown(SoftwareShutdownMode::Normal).unwrap();
+    let mut led_c = Is31fl32xx::<IS31FL3236, _>::init_with_i2c(0x00, i2c_dev2);
+    led_c.set_global_output(GlobalEnable::Enable).unwrap();
+    led_c.set_shutdown(SoftwareShutdownMode::Normal).unwrap();
 
     let i2c_dev1 = bus.acquire_i2c();
     let mut rtc = Ds323x::new_ds3231(i2c_dev1);
@@ -65,9 +55,7 @@ async fn main(_spawner: Spawner) {
     rtc.use_int_sqw_output_as_square_wave().unwrap();
     rtc.enable_square_wave().unwrap();
 
-    // TODO: check if the clock already has a time (from backup battery)
     info!("build time: {}", build_time());
-
     let dt = rtc.datetime().unwrap();
     if dt.year() < 2020 {
         info!("setting time");
@@ -83,7 +71,7 @@ async fn main(_spawner: Spawner) {
         let dt = rtc.datetime().unwrap();
         let temp = rtc.temperature().unwrap();
         let timestamp = dt.and_utc().timestamp();
-        info!("time: {}:{}:{}", dt.hour(), dt.minute(), dt.second());
+        info!("time: {}:{}:{} at {} C", dt.hour(), dt.minute(), dt.second(), temp);
 
         // iterate over 32 LEDs
         for n in 0..36 {
@@ -92,8 +80,8 @@ async fn main(_spawner: Spawner) {
             } else {
                 OutputMode::LEDOff
             };
-            ledC.set_led(n, OutputCurrent::IMaxDiv3, output_mode).unwrap();
-            ledC.set_pwm(n, 0x1f as u8).unwrap();
+            led_c.set_led(n, OutputCurrent::IMaxDiv3, output_mode).unwrap();
+            led_c.set_pwm(n, 0x1f as u8).unwrap();
         }
     }
 }
